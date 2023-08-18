a41dbbe242500b1feb5d0fbea8220fe6813cbc03
sarthaklambaa <samlamba29@gmail.com>
2023-08-18 23:02:31 +0530

    Added scrolling to ground image

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
 screen_width = 864
 screen_height = 936
 
@@ -20,9 +23,13 @@ ground_bg = pygame.image.load('img/bg-ground.png')
 run = True
 while run:
 
+    clock.tick(fps)
+
     screen.blit(bg, (0,0))
     screen.blit(ground_bg, (ground_scroll,768))
     ground_scroll -= scroll_speed
+    if abs(ground_scroll) > 35:
+        ground_scroll = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

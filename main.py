d5aa46912dd06240448b9a52c0796a7e20a6b159
sarthaklambaa <samlamba29@gmail.com>
2023-08-20 20:38:45 +0530

    Added Pipe

--- main.py
+++ main.py
@@ -73,11 +73,22 @@ class Bird(pygame.sprite.Sprite):
             self.image = pygame.transform.rotate(self.images[self.index], -90)
 
 
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/pipe.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x,y]
+
 bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
 
 flappy = Bird(100, int(screen_height / 2))
-
 bird_group.add(flappy)
+
+btm_pipe =  Pipe(300, int(screen_height / 2))
+pipe_group.add(btm_pipe)
 
 run = True
 while run:
@@ -88,6 +99,9 @@ while run:
 
     bird_group.draw(screen)
     bird_group.update()
+
+    pipe_group.draw(screen)
+    pipe_group.update()
 
     # Drawing the ground
     screen.blit(ground_bg, (ground_scroll,768))
